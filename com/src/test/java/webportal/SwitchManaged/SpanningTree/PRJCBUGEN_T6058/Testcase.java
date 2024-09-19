6b9c2b0f5700e738521ca4dcf65ebbfac3a0382d
tvishwanath <tvishwanath@netgear.com>
2024-09-19 12:43:26 +0530

    SWITCH PRIORITY

--- com/src/test/java/webportal/SwitchManaged/SpanningTree/PRJCBUGEN_T6058/Testcase.java
+++ com/src/test/java/webportal/SwitchManaged/SpanningTree/PRJCBUGEN_T6058/Testcase.java
@@ -32,7 +32,7 @@ public class Testcase extends TestCaseBase {
     @Description("008-Set port STP status on physical port and verify the function under RSTP protocol") // It's a testcase title from Jira Test Case.
     @TmsLink("PRJCBUGEN-T6058") // It's a testcase id/link from Jira Test Case.
     
-    @Test(alwaysRun = true, groups = "p2") // Use p1/p2/p3 to high/normal/low on priority
+    @Test(alwaysRun = true, groups = "p1") // Use p1/p2/p3 to high/normal/low on priority
     public void test() throws Exception {
         runTest(this);
     }
--- com/src/test/java/webportal/SwitchManaged/SpanningTree/PRJCBUGEN_T6059/Testcase.java
+++ com/src/test/java/webportal/SwitchManaged/SpanningTree/PRJCBUGEN_T6059/Testcase.java
@@ -32,7 +32,7 @@ public class Testcase extends TestCaseBase {
     @Description("010-Set port STP status on LAG port and verify the function under RSTP protocol") // It's a testcase title from Jira Test Case.
     @TmsLink("PRJCBUGEN-T6059") // It's a testcase id/link from Jira Test Case.
 
-    @Test(alwaysRun = true, groups = "p2") // Use p1/p2/p3 to high/normal/low on priority
+    @Test(alwaysRun = true, groups = "p1") // Use p1/p2/p3 to high/normal/low on priority
     public void test() throws Exception {
         runTest(this);
     }
