f3b8a971e283823fd55d3b0e9b647408a42f6ca2
tejeshwini <tvishwanath@netgear.com>
2024-09-30 12:29:57 +0530

    VLAN testcases, method to make switch onlione and offline

--- com/src/test/java/webportal/SwitchManaged/VLAN/PRJCBUGEN_T4704/Testcase.java
+++ com/src/test/java/webportal/SwitchManaged/VLAN/PRJCBUGEN_T4704/Testcase.java
@@ -11,6 +11,7 @@ import io.qameta.allure.Step;
 import io.qameta.allure.Story;
 import io.qameta.allure.TmsLink;
 import testbase.TestCaseBase;
+import util.MyCommonAPIs;
 import util.SwitchCLIUtils;
 import webportal.weboperation.WebportalLoginPage;
 import webportal.weboperation.WiredQuickViewPage;
@@ -39,7 +40,8 @@ public class Testcase extends TestCaseBase {
     @AfterMethod(alwaysRun = true)
     public void restore() {
         System.out.println("start to do restore");
-        SwitchCLIUtils.CloudModeSet(true);
+//        SwitchCLIUtils.CloudModeSet(true);
+        SwitchCLIUtils.SwitchOfflineOnline("Connect");
         WiredQuickViewPage wiredQuickViewPage = new WiredQuickViewPage();
         WiredVLANPageForVLANPage vlanPage = new WiredVLANPageForVLANPage();
         vlanPage.deleteAllVlan();
@@ -47,10 +49,10 @@ public class Testcase extends TestCaseBase {
 
     @Step("Test Step 1: Put DUT out of internet")
     public void step1() {
-        SwitchCLIUtils.CloudModeSet(false);
-        
-        SwitchCLIUtils.CloudModeSet(true);
-        handle.sleepi(5 * 60);
+//        SwitchCLIUtils.CloudModeSet(false);
+        SwitchCLIUtils.SwitchOfflineOnline("Disconnect");
+//        SwitchCLIUtils.CloudModeSet(true);
+//        handle.sleepi(5 * 60);
     }
 
     // Each step is a single test step from Jira Test Case
@@ -70,6 +72,7 @@ public class Testcase extends TestCaseBase {
     // Each step is a single test step from Jira Test Case
     @Step("Test Step 3: Check vlan 100 on switch")
     public void step3() {
+        MyCommonAPIs.sleepsync();
         String sRet = handle.waitCmdReady(vlanId, false);
         System.out.println(sRet);
         assertTrue(sRet.contains(vlanId), "check for " + vlanId);
