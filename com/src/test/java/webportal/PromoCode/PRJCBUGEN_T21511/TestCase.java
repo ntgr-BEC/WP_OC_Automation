2c70cda37ed39510594e29d483aedf9e8c8d852c
sumanta <sumanta.jena@netgear.com>
2024-10-23 08:35:18 +0530

    Promocode

--- com/src/test/java/webportal/PromoCode/PRJCBUGEN_T21511/TestCase.java
+++ com/src/test/java/webportal/PromoCode/PRJCBUGEN_T21511/TestCase.java
@@ -7,6 +7,7 @@ import java.util.Map;
 import java.util.Random;
 
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import io.qameta.allure.Description;
@@ -20,6 +21,7 @@ import webportal.publicstep.UserManage;
 import webportal.weboperation.AccountPage;
 import webportal.weboperation.DevicesDashPage;
 import webportal.weboperation.HamburgerMenuPage;
+import webportal.weboperation.PostManPage;
 import webportal.weboperation.WebportalLoginPage;
 
 public class TestCase extends TestCaseBase {
@@ -40,6 +42,13 @@ public class TestCase extends TestCaseBase {
         runTest(this);
     }
 
+    @BeforeMethod(alwaysRun = true)
+    public void tearUp() {
+       
+       new PostManPage().Deregister(WebportalParam.ap5serialNo);
+        
+    }
+    
     @AfterMethod(alwaysRun = true)
     public void tearDown() {
         new AccountPage().deleteOneLocation("OnBoardingTest");
