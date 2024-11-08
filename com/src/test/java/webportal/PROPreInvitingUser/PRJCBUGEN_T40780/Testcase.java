57f3012b2f541e72dc03ae4a8a1d61109a8b7c27
sumanta <sumanta.jena@netgear.com>
2024-11-08 18:23:27 +0530

    Propreinviteuser Pratik

--- com/src/test/java/webportal/PROPreInvitingUser/PRJCBUGEN_T40780/Testcase.java
+++ com/src/test/java/webportal/PROPreInvitingUser/PRJCBUGEN_T40780/Testcase.java
@@ -1,4 +1,4 @@
-package webportal.PROPreInvitingUser.PRJCBUGEN_T40773;
+package webportal.PROPreInvitingUser.PRJCBUGEN_T40780;
 
 import static org.testng.Assert.assertTrue;
 
@@ -30,14 +30,14 @@ public class Testcase extends TestCaseBase {
 
     Random r        = new Random();
     int    num      = r.nextInt(10000000);
-    String mailname = "apwptest" + String.valueOf(num);
+    String mailname = "owner" + String.valueOf(num);
     String ownMail  = mailname + "@yopmail.com";
-    String organizationName = "PRJCBUGEN_T40773";
+    String organizationName = "PRJCBUGEN_T40780";
 
     @Feature("PROPreInvitingUser") // It's a folder/component name to make test suite more readable from Jira Test Case.
-    @Story("PRJCBUGEN_T40773") // It's a testcase id/link from Jira Test Case but replace - with _.
-    @Description("Test to verify a user is able to Invite the Owner.") // It's a testcase title from Jira Test Case.
-    @TmsLink("PRJCBUGEN_T40773") // It's a testcase id/link from Jira Test Case.
+    @Story("PRJCBUGEN_T40780") // It's a testcase id/link from Jira Test Case but replace - with _.
+    @Description("Test to verify a user is able to get the invitation link on email for the again invited Owner.") // It's a testcase title from Jira Test Case.
+    @TmsLink("PRJCBUGEN_T40780") // It's a testcase id/link from Jira Test Case.
 
     @Test(alwaysRun = true, groups = "p1") // Use p1/p2/p3 to high/normal/low on priority
     public void test() throws Exception {
@@ -46,6 +46,8 @@ public class Testcase extends TestCaseBase {
 
     @AfterMethod(alwaysRun = true)
     public void tearDown() {
+        WebportalLoginPage webportalLoginPage = new WebportalLoginPage(true);
+        webportalLoginPage.loginByUserPassword(WebportalParam.adminName, WebportalParam.adminPassword);
         new OrganizationPage().deleteOrganizationNew(organizationName);
         System.out.println("start to do tearDown");
     }
@@ -58,29 +60,30 @@ public class Testcase extends TestCaseBase {
 
     }
 
-    @Step("Test Step 2: Create a organization and change organization owner information, then check its changed;")
+    @Step("Test Step 2: Invite manager and check its success;")
     public void step2() {
         Map<String, String> organizationInfo = new HashMap<String, String>();
         organizationInfo.put("Name", organizationName);
         organizationInfo.put("Owner Name", "test14345");
-        organizationInfo.put("Email Address", mailname + "@yopmail.com");
+        organizationInfo.put("Email Address", ownMail);
         organizationInfo.put("Phone Number", "12345678910");
         organizationInfo.put("Business Phone Number", "10987654321");
 
         OrganizationPage OrganizationPage = new OrganizationPage();
         OrganizationPage.addOrganization(organizationInfo);
-
-        if (OrganizationPage.checkOrganizationIsExist(organizationName) && OrganizationPage.checkOrganizationOwner(organizationInfo)) {
-            organizationInfo.put("Email Address", mailname + "22@yopmail.com");
-            organizationInfo.put("Phone Number", "10987654321");
-            organizationInfo.put("Business Phone Number", "12345678910");
-            new OrganizationPage().editOrganization(organizationInfo);
-
-            assertTrue(OrganizationPage.checkOrganizationOwner(organizationInfo), "Organization owner information not changed.");
-        } else {
-            assertTrue(false, "Organization not created.");
-        }
-     
+    } 
+    
+    @Step("Test Step 3: Invite manager and check its success;")
+    public void step3() {
+        
+        new OrganizationPage().deleteOrganizationNew(organizationName);
+        Map<String, String> organizationInfo = new HashMap<String, String>();
+        organizationInfo.put("Name", organizationName);
+        OrganizationPage OrganizationPage = new OrganizationPage();
+        OrganizationPage.addOrganization(organizationInfo);
+        organizationInfo.put("Email Address", ownMail);
+        new OrganizationPage().selectExistingOwner(organizationInfo);
+        assertTrue(new HamburgerMenuPage(false).checkEmailMessageForInvitemangaerOwner(ownMail), "Not received Invitation email.");
+        
     }
-
 }
