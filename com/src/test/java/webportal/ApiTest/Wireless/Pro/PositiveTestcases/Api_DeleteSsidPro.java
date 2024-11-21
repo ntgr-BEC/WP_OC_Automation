ffb831b6cc643454b534514930794d46a459fe78
tejeshwini <tvishwanath@netgear.com>
2024-11-21 18:28:34 +0530

    API folder

---
+++ com/src/test/java/webportal/ApiTest/Wireless/Pro/PositiveTestcases/Api_DeleteSsidPro.java
@@ -0,0 +1,79 @@
+package webportal.ApiTest.Wireless.Pro.PositiveTestcases;
+import static org.hamcrest.CoreMatchers.equalTo;
+
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import io.qameta.allure.Description;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Step;
+import io.qameta.allure.Story;
+import io.qameta.allure.TmsLink;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import testbase.TestCaseBaseApi;
+import util.MyCommonAPIs;
+import webportal.ApiTest.Location.PositiveTestcases.Api_AddNetwork;
+//import webportal.weboperation.WirelessQuickViewPage;
+import webportal.param.WebportalParam;
+import webportal.weboperation.ApiRequest;
+
+import static io.restassured.RestAssured.*;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+
+
+public class Api_DeleteSsidPro extends TestCaseBaseApi{
+
+    Map<String, String> endPointUrl = new HashMap<String, String>();
+    Map<String, String> pathParams = new HashMap<String, String>();
+    Map<String, String> headers = new HashMap<String, String>();
+    String wirelessOrgId;
+   
+    
+    @Feature("Api_AddSsidPro") // It's a folder/component name to make test suite more readable from Jira Test Case.
+    @Story("PRJCBUGEN_T004") // It's a testcase id/link from Jira Test Case but replace - with _.
+    @Description("Addition of ssid to pro account") // It's a testcase title from Jira Test Case.
+    @TmsLink("PRJCBUGEN_T004") // It's a testcase id/link from Jira Test Case.
+    
+    @Test(alwaysRun = true, groups = "p1") // Use p1/p2/p3 to high/normal/low on priority
+    public void test() throws Exception {
+        step1();
+   }
+    
+    @BeforeMethod(alwaysRun=true)
+    public void teardown()
+    { 
+         Response add = new Api_AddSsidPro().step1();
+         wirelessOrgId= add.jsonPath().getString("wirelessOrgInfo.wirelessOrgId");
+    }
+    
+
+    @Step("Send get request to {url}")
+    public Response step1()
+    { 
+        endPointUrl = new ApiRequest().ENDPOINT_URL;
+       
+        headers.put("token",WebportalParam.token);
+        headers.put("apikey",WebportalParam.apikey);    
+        headers.put("accountId",WebportalParam.accountId);
+       
+        pathParams.put("orgId",WebportalParam.orgId);
+        pathParams.put("wirelessOrgId",wirelessOrgId);
+        //TO PERFORM ANY REQUEST
+    
+        Response getResponse = ApiRequest.sendDeleteRequest(endPointUrl.get("Delete_Ssid_Pro"),headers, pathParams, null); 
+        getResponse.then().body("response.status", equalTo(true))
+         .body("orgNwInfo[0].message", equalTo("Applying your configuration settings. This can take up to 3 minutes to display"));
+        
+        return getResponse;
+    }
+                  
+    }
+
