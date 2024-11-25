863e925c3c8afd80770558518931d19a37c79c04
teju <tvishwanath@netgear.com>
2024-11-25 10:20:37 +0530

    Api_code_Anu

---
+++ com/src/test/java/webportal/ApiTest/Wireless/Pro/NegativeTestcases/ModifySsidProWithInvalidBody_Api.java
@@ -0,0 +1,89 @@
+package webportal.ApiTest.Wireless.Pro.NegativeTestcases;
+import static org.hamcrest.CoreMatchers.equalTo;
+
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import io.qameta.allure.Description;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Step;
+import io.qameta.allure.Story;
+import io.qameta.allure.TmsLink;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import testbase.TestCaseBaseApi;
+import util.MyCommonAPIs;
+import webportal.ApiTest.Location.PositiveTestcases.Api_AddNetwork;
+import webportal.ApiTest.Wireless.PositiveTestcases.Api_AddSsid;
+import webportal.ApiTest.Wireless.Pro.PositiveTestcases.Api_AddSsidPro;
+//import webportal.weboperation.WirelessQuickViewPage;
+import webportal.param.WebportalParam;
+import webportal.weboperation.ApiRequest;
+
+import static io.restassured.RestAssured.*;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+
+
+public class ModifySsidProWithInvalidBody_Api extends TestCaseBaseApi{
+
+    Map<String, String> endPointUrl = new HashMap<String, String>();
+  
+    Map<String, String> headers = new HashMap<String, String>();
+    String wirelessOrgId;
+    
+    @Feature("ModifySsidProWithInvalidBody_Api") // It's a folder/component name to make test suite more readable from Jira Test Case.
+    @Story("PRJCBUGEN_T004") // It's a testcase id/link from Jira Test Case but replace - with _.
+    @Description("Modify of ssid to pro account with invalid body") // It's a testcase title from Jira Test Case.
+    @TmsLink("PRJCBUGEN_T004") // It's a testcase id/link from Jira Test Case.
+    
+    @Test(alwaysRun = true, groups = "p1") // Use p1/p2/p3 to high/normal/low on priority
+    public void test() throws Exception {
+        step1();
+   }
+    
+    @AfterMethod(alwaysRun=true)
+    public void teardown()
+    { 
+        Map<String, String> pathParams = new HashMap<String, String>();
+        pathParams.put("orgId",WebportalParam.orgId);
+        pathParams.put("wirelessOrgId",wirelessOrgId);
+        
+        Response getResponse = ApiRequest.sendDeleteRequest(endPointUrl.get("Delete_Ssid_Pro"),headers, pathParams, null); 
+        getResponse.then().body("response.status", equalTo(true));
+
+    }
+   
+
+    @Step("Send get request to {url}")
+    public void step1()
+    { 
+        endPointUrl = new ApiRequest().ENDPOINT_URL;
+        Response add= new Api_AddSsidPro().step1();
+        wirelessOrgId=add.jsonPath().getString("wirelessOrgInfo.wirelessOrgId");
+        headers.put("token",WebportalParam.token);
+        headers.put("apikey",WebportalParam.apikey);    
+        headers.put("accountId",WebportalParam.accountId);
+       
+        Map<String, String> pathParams = new HashMap<String, String>();
+        pathParams.put("orgId",WebportalParam.orgId);
+        pathParams.put("wirelessOrgId",wirelessOrgId);
+        
+        String InvalidmodifyOrgName = "Org\\Modify";
+        String InvalidmodifyPassword = "";
+        String requestBody="{\"wirelessNetwork\":{\"mloStatus\":\"0\",\"enable\":\"1\",\"ssid\":\""+InvalidmodifyOrgName+"\",\"vlanId\":\"1\",\"vlanType\":1,\"radioBand\":\"12\",\"broadcastStatus\":\"0\",\"bandSteeringSt\":\"0\",\"clientIsoSt\":\"0\",\"allowAccessToCIList\":\"0\",\"ciAllowedList\":[],\"securitySt\":\"0\",\"rrmSt\":\"0\",\"security\":{\"authentication\":\"80\",\"password\":\""+InvalidmodifyPassword+"\",\"oweMode\":\"0\"},\"rateLimit\":{\"enableRateLimit\":0,\"uploadRateLimit\":\"10000000\",\"downloadRateLimit\":\"10000000\"},\"captivePortal\":{\"enableCaptivePortal\":\"0\"},\"fbCpStatus\":\"0\",\"accessToApSt\":\"0\",\"dynamicVlanSt\":\"0\",\"fastRoamingSt\":\"0\",\"kvrStatus\":\"1\",\"arsStatus\":\"0\",\"encryption\":\"0\",\"bandChanged\":\"0\",\"vlanChanged\":\"0\",\"nameChanged\":\"0\",\"natMode\":{\"status\":\"0\",\"networkAddress\":\"\",\"subnet\":\"255.255.252.0\",\"dns\":\"8.8.8.8\",\"leaseTime\":1440},\"iotRadiusServer\":\"0\",\"iotRadiusPolicyId\":\"\",\"iotRadiusServerId\":\"\",\"mduStatus\":\"0\",\"mdnsEnabled\":false,\"ars\":{\"wlan0\":{\"status\":\"0\",\"densityLevel\":\"0\",\"multicastRate\":\"11\"},\"wlan1\":{\"status\":\"0\",\"densityLevel\":\"0\",\"multicastRate\":\"24\"}}}}";       
+        //will modify the ssid with 3 band, name, password and security is wpa3 personal
+        //TO PERFORM ANY REQUEST
+    
+        Response getResponse = ApiRequest.sendPutRequest(endPointUrl.get("Modify_SsidPro"), requestBody, headers, pathParams, null,400); 
+        getResponse.then().body("response.status", equalTo(false));
+      
+    }
+}
+  
