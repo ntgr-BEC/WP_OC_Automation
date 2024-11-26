90da8e35d0b46eabdd350e9e343c9114c88b7bf3
tejeshwini <tvishwanath@netgear.com>
2024-11-26 16:02:39 +0530

    Api_ravi_pro

--- com/src/test/java/webportal/ApiTest/Location/Pro/PositiveTestcases/Api_GetListOfNetwork.java
+++ com/src/test/java/webportal/ApiTest/Location/Pro/PositiveTestcases/Api_GetListOfNetwork.java
@@ -53,7 +53,7 @@ public class Api_GetListOfNetwork extends TestCaseBaseApi{
     { 
         Map<String, String> pathParams = new HashMap<String, String>();
         pathParams.put("orgId",  OrgID);
-        pathParams.put("accountId",WebportalParam.accountId);
+        pathParams.put("accountId",WebportalParam.accountIdPro);
         
         Response getResponse1 = ApiRequest.sendDeleteRequest(endPointUrl.get("Delete_Organization"), headers, pathParams, null); 
         getResponse1.then().body("response.status", equalTo(true));
@@ -83,11 +83,11 @@ public class Api_GetListOfNetwork extends TestCaseBaseApi{
         endPointUrl = new ApiRequest().ENDPOINT_URL; 
         
         headers.put("apikey",WebportalParam.apikey);
-        headers.put("token",WebportalParam.token);
+        headers.put("token",WebportalParam.tokenPro);
         
         
         Map<String, String> pathParams = new HashMap<String, String>();
-        pathParams.put("accountId",WebportalParam.accountId);
+        pathParams.put("accountId",WebportalParam.accountIdPro);
         pathParams.put("orgId",OrgID);
         
         Response getResponse = ApiRequest.sendGetRequest(endPointUrl.get("Get_Network_Pro"), headers, pathParams, null); 
