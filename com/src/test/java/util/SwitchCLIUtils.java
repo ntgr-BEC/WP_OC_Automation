6719e0f20fa5508c837ac8f429cd3e6b3a6a5905
tejeshwini <tvishwanath@netgear.com>
2024-11-26 13:12:45 +0530

    SNMP

--- com/src/test/java/util/SwitchCLIUtils.java
+++ com/src/test/java/util/SwitchCLIUtils.java
@@ -1380,14 +1380,14 @@ public class SwitchCLIUtils {
                     isTrapEnable = true;
                 }
             } else {
-                if (res1.contains("Disable")) {
+                if (res1.contains("no snmp-server enable traps")) {
                     isTrapEnable = false;
                 } else {
                     isTrapEnable = true;
                 }
             }
             
-            if (res2.contains(" ro") || res2.contains("Read Only") || res1.contains("Read Only")) {
+            if (res2.contains("ro") || res2.contains("Read Only") || res1.contains("Read Only")) {
                 isReadyOnly = true;
             } else {
                 isReadyOnly = false;
@@ -1410,10 +1410,13 @@ public class SwitchCLIUtils {
             
             if(WebportalParam.sw1Model.contains("M4")) {
                 
-                retTem = st.sendCLICommandClear2("show running-config", null); //retTem = st.sendCLICommandClear("show running-config", null);
-                trapRes = WebportalParam.includeLines(retTem, "Authentication trap", false);
-                commRes = WebportalParam.includeLines(retTem, "snmp community", false);
+                retTem = st.sendCLICommandClear2("show running-config", "snmp"); //retTem = st.sendCLICommandClear("show running-config", null);
+                trapRes = WebportalParam.includeLines(retTem, "traps", false);
+                commRes = WebportalParam.includeLines(retTem, "snmp-server community", false);
                 commRes += WebportalParam.includeLines(retTem, "snmp host", false);
+                System.out.println("trapRes" +trapRes);
+                System.out.println("commRes" +commRes);
+                
                 
             }else {
             trapRes = st.sendCLICommandClear("show snmptrap", null);
