7b62814f8574e0137fd171748026510dbcc07fe7
tejeshwini <tvishwanath@netgear.com>
2024-11-27 17:24:53 +0530

    Switch

--- com/src/test/java/webportal/SwitchManaged/VlanOverhauling/PRJCBUGEN_T11328/Testcase.java
+++ com/src/test/java/webportal/SwitchManaged/VlanOverhauling/PRJCBUGEN_T11328/Testcase.java
@@ -54,6 +54,9 @@ public class Testcase extends TestCaseBase {
 
     @Step("Test Step 2: Insight APP go to Network Level Configuration, Create VLAN via default template of Voice VLAN")
     public void step2() {
+        WiredQuickViewPage wiredQuickViewPage = new WiredQuickViewPage();
+        WiredVLANPageForVLANPage vlanPage = new WiredVLANPageForVLANPage();
+        vlanPage.deleteAllVlan();
         netsp.gotoPage();
         netsp.createNetwork(networkName, 1, vlanName, vlanId);
         handle.waitCmdReady(vlanId, false);
