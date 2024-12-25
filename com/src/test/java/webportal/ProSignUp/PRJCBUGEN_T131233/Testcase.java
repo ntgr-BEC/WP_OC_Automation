94657929c35981f5034600e1a2b754976abe5866
sumanta <https://github.netgear.com/BEC/Web_Portal_Automation>
2024-12-25 11:54:50 +0530

    Pro Sign Up New Changes

--- com/src/test/java/webportal/ProSignUp/PRJCBUGEN_T131233/Testcase.java
+++ com/src/test/java/webportal/ProSignUp/PRJCBUGEN_T131233/Testcase.java
@@ -77,6 +77,8 @@ public class Testcase extends TestCaseBase {
                 businessInfo.put("Zip Code", "6320");
                 businessInfo.put("Country", "Slovenia");
                 businessInfo.put("Business Phone Number", "1234567890");
+                businessInfo.put("Confirm Email", mailname);
+                businessInfo.put("Password", "Netgear1@");
                 
                 assertTrue(new HamburgerMenuPage(false).addingOneYearInsightDevice(businessInfo)," Pro Account creation failed through flow Adding Pro Subscription Key ");
                 assertTrue(new HamburgerMenuPage(false).checkLoginSuccessful(), "Create pro account unsuccess.");      
