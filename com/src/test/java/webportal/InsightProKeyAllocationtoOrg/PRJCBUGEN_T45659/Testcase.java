4c7b5fa6e3d84dbcb5a37a263e69012659650f0c
sumanta <https://github.netgear.com/BEC/Web_Portal_Automation>
2025-01-07 13:04:12 +0530

    CreditManagement_Pratik_NewTestCases_Added

---
+++ com/src/test/java/webportal/InsightProKeyAllocationtoOrg/PRJCBUGEN_T45659/Testcase.java
@@ -0,0 +1,77 @@
+package webportal.InsightProKeyAllocationtoOrg.PRJCBUGEN_T45659;
+
+import static org.testng.Assert.assertTrue;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.Test;
+
+import io.qameta.allure.Description;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Step;
+import io.qameta.allure.Story;
+import io.qameta.allure.TmsLink;
+import testbase.TestCaseBase;
+import webportal.param.WebportalParam;
+import webportal.weboperation.HamburgerMenuPage;
+import webportal.weboperation.OrganizationPage;
+import webportal.weboperation.WebportalLoginPage;
+
+/**
+ *
+ * @author dallas
+ *
+ */
+public class Testcase extends TestCaseBase {
+
+    String              organizationName    = "PRJCBUGEN_T45659";
+    Map<String, String> organizationInfo    = new HashMap<String, String>();
+    int                 devNum              = 3;
+    int                 icpNum              = 2;
+
+    @Feature("InsightProKeyAllocationtoOrg") // It's a folder/component name to make test suite more readable from Jira Test Case.
+    @Story("PRJCBUGEN_T45659") // It's a testcase id/link from Jira Test Case but replace - with _.
+    @Description("Verify the Deallocation of Credit.") // It's a test case title from Jira Test Case.
+    @TmsLink("PRJCBUGEN_T45659") // It's a testcase id/link from Jira Test Case.
+
+    @Test(alwaysRun = true, groups = "p2") // Use p1/p2/p3 to high/normal/low on priority
+    public void test() throws Exception {
+        runTest(this);
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void tearDown() {
+        System.out.println("start to do tearDown");
+        new OrganizationPage().deleteOrganizationNew(organizationName);
+    }
+
+    // Each step is a single test step from Jira Test Case
+    @Step("Test Step 1: Login IM WP success;")
+    public void step1() {
+        WebportalLoginPage webportalLoginPage = new WebportalLoginPage(true);
+        webportalLoginPage.loginByUserPassword(WebportalParam.adminName, WebportalParam.adminPassword);
+    }
+
+    @Step("Test Step 2: Add new organization, then check devices credits page;")
+    public void step2() {
+        organizationInfo.put("Name", organizationName);
+
+        OrganizationPage OrganizationPage = new OrganizationPage();
+        OrganizationPage.addOrganization(organizationInfo);
+
+        new HamburgerMenuPage().configCreditAllocation(organizationName, devNum, 0, icpNum);
+
+    }
+
+    @Step("Test Step 3: Deallocate credits, then check devices credits page;")
+    public void step3() {
+        new HamburgerMenuPage().deallocateCredit(organizationName, String.valueOf(devNum), String.valueOf(icpNum));
+
+        HashMap<String, String> creditsInfo = new HamburgerMenuPage().getCreditAllocationStatus(organizationName);
+        assertTrue(creditsInfo.get("Used Devices Credits").equals("0") && creditsInfo.get("Unused Devices Credits").equals("0")
+                && creditsInfo.get("Used ICP Credits").equals("0") && creditsInfo.get("Unused ICP Credits").equals("0"));
+    }
+
+}
