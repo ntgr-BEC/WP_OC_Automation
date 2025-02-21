7a69e10019c211d8922d911da10871d2e7c7f432
tejeshwini <tvishwanath@netgear.com>
2025-02-21 12:07:59 +0530

    RAVI_COMMIT

--- com/src/test/java/webportal/Premium_To_Pro_Migration/ICP/PRJCBUGEN_T33708/Testcase.java
+++ com/src/test/java/webportal/Premium_To_Pro_Migration/ICP/PRJCBUGEN_T33708/Testcase.java
@@ -1,4 +1,4 @@
-package webportal.Premium_To_Pro_Migration.PRJCBUGEN_T33707;
+package webportal.Premium_To_Pro_Migration.ICP.PRJCBUGEN_T33708;
 
 import static org.testng.Assert.assertTrue;
 
@@ -39,12 +39,12 @@ public class Testcase extends TestCaseBase {
     Random r = new Random();
     int num = r.nextInt(10000000);
     String mailname = "apwptest" + String.valueOf(num);
-    String organizationName = "PRJCBUGEN_T33707";
+    String organizationName = "PRJCBUGEN_T33708";
 
     @Feature("Premium_To_Pro_Migration") // It's a folder/component name to make test suite more readable from Jira Test Case.
-    @Story("PRJCBUGEN_T33707") // It's a testcase id/link from Jira Test Case but replace - with _.
-    @Description("Test to verify, user should able to puchase multiple instant captive portal for diff-diff year.") // It's a testcase title from Jira Test Case.
-    @TmsLink("PRJCBUGEN_T33707") // It's a testcase id/link from Jira Test Case.
+    @Story("PRJCBUGEN_T33708") // It's a testcase id/link from Jira Test Case but replace - with _.
+    @Description("Test to verify that ,user should able to see added multple ICP pack in ICP list.") // It's a testcase title from Jira Test Case.
+    @TmsLink("PRJCBUGEN_T33708") // It's a testcase id/link from Jira Test Case.
 
     @Test(alwaysRun = true, groups = "p2") // Use p1/p2/p3 to high/normal/low on priority
     public void test() throws Exception {
@@ -62,6 +62,7 @@ public class Testcase extends TestCaseBase {
     @AfterMethod(alwaysRun = true)
     public void tearDown() {
         new AccountPage().deleteOneLocation("OnBoardingTest");
+        //new OrganizationPage().deleteOrganizationNew(organizationName);
         System.out.println("start to do tearDown");
     }
 
@@ -85,7 +86,7 @@ public class Testcase extends TestCaseBase {
         new HamburgerMenuPage(false).createAccount(accountInfo);
     }
     
-    @Step("Test Step 2: Create New location;")
+    @Step("Test Step 2: Create new location;")
     public void step2() {
         
 //      new HamburgerMenuPage(false).clickAddInsightIncludedDevices();
@@ -93,25 +94,23 @@ public class Testcase extends TestCaseBase {
         HashMap<String, String> locationInfo = new HashMap<String, String>();
         locationInfo.put("Location Name", "OnBoardingTest");
         locationInfo.put("Device Admin Password", WebportalParam.loginDevicePassword);
-        locationInfo.put("Zip Code", "4560");
-        locationInfo.put("Country", "Australia");
+        locationInfo.put("Zip Code", "32003");
+        locationInfo.put("Country", "United States of America");
         new AccountPage().addNetwork(locationInfo);
         
     }
     
     
-    @Step("Test Step 3: Add device To the Network;")
+    @Step("Test Step 3: Add dummy non hardbundle device To the Network;")
     public void step3() {
         
         new AccountPage(false).enterLocation(WebportalParam.location1);
         
-         new AccountPage().enterLocation("OnBoardingTest");
-        
-        Map<String, String> firststdevInfo = new HashMap<String, String>();
+         Map<String, String> firststdevInfo = new HashMap<String, String>();
        
         
-        firststdevInfo.put("Serial Number1", WebportalParam.ap5serialNo);
-        firststdevInfo.put("MAC Address1", WebportalParam.ap5macaddress);
+        firststdevInfo.put("Serial Number1", WebportalParam.ap6serialNo);
+        firststdevInfo.put("MAC Address1", WebportalParam.ap6macaddress);
         
         System.out.println(firststdevInfo);
  
@@ -120,7 +119,7 @@ public class Testcase extends TestCaseBase {
         
     }
     
-    @Step("Test Step 4: Add diff diff icp credits;")
+    @Step("Test Step 4: Add icp credits;")
     public void step4() {
         
 //      new HamburgerMenuPage(false).clickAddInsightIncludedDevices();
@@ -129,7 +128,7 @@ public class Testcase extends TestCaseBase {
         CaptivePortalPaymentInfo.put("First Name", mailname);
         CaptivePortalPaymentInfo.put("Last Name", "T23919");
         CaptivePortalPaymentInfo.put("Email", mailname + "@mailinator.com");
-        CaptivePortalPaymentInfo.put("Quantity", "1"); // can input 1 , 3 , 10 , 40
+        CaptivePortalPaymentInfo.put("Quantity", "10"); // can input 1 , 3 , 10 , 40
         CaptivePortalPaymentInfo.put("Duration", "1"); // can input 1 , 3
         CaptivePortalPaymentInfo.put("Street Address", "Springs Rd");
         CaptivePortalPaymentInfo.put("City", "Red Bank");
@@ -139,26 +138,9 @@ public class Testcase extends TestCaseBase {
         new InsightServicesPage().buyCaptivePortalProducts(CaptivePortalPaymentInfo);
         MyCommonAPIs.sleepi(10);
         boolean result = false;
+        handle.gotoLoction();
         result = new HamburgerMenuPage().checkCaptivePortalServicesCredits();
         assertTrue(result, "<2> Captive portal services credits is incorrect.");
-        
-        CaptivePortalPaymentInfo = new CommonDataType().CARD_INFO;
-        CaptivePortalPaymentInfo.put("First Name", mailname);
-        CaptivePortalPaymentInfo.put("Last Name", "T23919");
-        CaptivePortalPaymentInfo.put("Email", mailname + "@mailinator.com");
-        CaptivePortalPaymentInfo.put("Quantity", "1"); // can input 1 , 3 , 10 , 40
-        CaptivePortalPaymentInfo.put("Duration", "3"); // can input 1 , 3
-        CaptivePortalPaymentInfo.put("Street Address", "Springs Rd");
-        CaptivePortalPaymentInfo.put("City", "Red Bank");
-        CaptivePortalPaymentInfo.put("Zip", "32003");
-        CaptivePortalPaymentInfo.put("Country", "US");
-        CaptivePortalPaymentInfo.put("State", "Florida");
-        new InsightServicesPage().buyCaptivePortalProducts(CaptivePortalPaymentInfo);
-        MyCommonAPIs.sleepi(10);
-        boolean result1 = false;
-        handle.gotoLoction();
-        result1 = new HamburgerMenuPage().checkCaptivePortalServicesCredits();
-        assertTrue(result1, "<2> Captive portal services credits is incorrect.");
         
     }
 }
