81f7bab7bc7e110ed27f98ff646d570128a1aac1
Sumanta <sjena@netgear.xom>
2025-03-11 20:28:39 +0530

    PR60X_PRO

---
+++ com/src/test/java/webportal/PR60X/IPSecVPNWizard/prouser/PRJCBUGEN_T38903/Testcase.java
@@ -0,0 +1,92 @@
+package webportal.PR60X.IPSecVPNWizard.prouser.PRJCBUGEN_T38903;
+
+import static org.testng.Assert.assertTrue;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.Test;
+
+import com.codeborne.selenide.Selenide;
+
+import io.qameta.allure.Description;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Step;
+import io.qameta.allure.Story;
+import io.qameta.allure.TmsLink;
+import testbase.TestCaseBase;
+import util.APUtils;
+import util.MyCommonAPIs;
+import util.RunCommand;
+import webportal.param.WebportalParam;
+import webportal.publicstep.UserManage;
+import webportal.weboperation.AccountPage;
+import webportal.weboperation.DevicesDashPage;
+import webportal.weboperation.HamburgerMenuPage;
+import webportal.weboperation.OrganizationPage;
+import webportal.weboperation.RoutersPage;
+import webportal.weboperation.WebportalLoginPage;
+import webportal.weboperation.WirelessQuickViewPage;
+
+/**
+ *
+ * @author Prathik
+ *
+ */
+public class Testcase extends TestCaseBase {
+
+    Map<String, String> locationInfo = new HashMap<String, String>();
+
+    @Feature("IPSecVPNWizard") // It's a folder/component name to make test suite more readable from Jira Test Case.
+    @Story("PRJCBUGEN_T38903") // It's a testcase id/link from Jira Test Case but replace - with _.
+    @Description("Verify the user is able to select the location while adding a spoke router.") // It's                                                                                                                                                     // Jira
+    // Test
+    // Case.
+    @TmsLink("PRJCBUGEN_T38903") // It's a testcase id/link from Jira Test Case.
+
+    @Test(alwaysRun = true, groups = "p1") // Use p1/p2/p3 to high/normal/low on priority
+    public void test() throws Exception {
+        runTest(this);
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void tearDown() {
+        System.out.println("start to do tearDown");
+    }
+
+    // Each step is a single test step from Jira Test Case
+    @Step("Test Step 1: Login to pro account success.")
+    public void step1() {
+
+        WebportalLoginPage webportalLoginPage = new WebportalLoginPage(true);
+        webportalLoginPage.loginByUserPassword(WebportalParam.adminName,WebportalParam.adminPassword);
+        new HamburgerMenuPage(false).closeLockedDialog();
+        new OrganizationPage(false).openOrg(WebportalParam.Organizations);
+
+        handle.gotoLoction();
+        // new DevicesDashPage().checkDeviceInAdminAccount();
+    }
+
+      @Step("Test Step 2: verfiy Spoke Router Has Location Listed under select network options")
+      public void step2() {
+      
+          new OrganizationPage(false).openOrg(WebportalParam.Organizations);
+          new MyCommonAPIs().gotoLoction(WebportalParam.location1);
+          new OrganizationPage(false).goWirelessSetting();
+          
+          Map<String, String> vpnGroupInfo = new HashMap<String, String>();
+          vpnGroupInfo.put("Group Name", "abcdefghi");
+          vpnGroupInfo.put("Description", "ertyui asdfgh ghjkl mnbvc kjhgwertyu");
+          vpnGroupInfo.put("Serial Number", WebportalParam.pr1serialNo);
+          vpnGroupInfo.put("LAN IP Address", "172.16.27.12");
+          vpnGroupInfo.put("LAN Subnet", "255.255.255.0");
+          vpnGroupInfo.put("Network Location", WebportalParam.location1);
+          assertTrue(new RoutersPage().verfiySpokeRouterHasLocationListed(vpnGroupInfo), "Spoke Router Has Location is not Listed under select network options");
+          
+      }
+}
