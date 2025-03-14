457f9b0c31ba5e316fd90d000153fc6484ad185c
tejeshwini <tvishwanath@netgear.com>
2025-03-14 12:29:31 +0530

    ICP testcases moved and removed checking for ICP credit

--- com/src/test/java/webportal/APBasic/WirelessSecurity/Location/Premium/PRJCBUGEN_T35755/Testcase.java
+++ com/src/test/java/webportal/APBasic/WirelessSecurity/Location/Premium/PRJCBUGEN_T35755/Testcase.java
@@ -62,9 +62,7 @@ public class Testcase extends TestCaseBase {
     @Step("Test Step 2: Add WIFI ssid and connect client")
     public void step2() {
         
-        if (!new HamburgerMenuPage().checkCaptivePortalServicesCredits()) {
-            assertTrue(false, "Account need to add instant captive portal key.");
-        }
+
 
         ssidInfo.put("SSID", "apwp35755");
         ssidInfo.put("Security", "WPA3 Personal Mixed (WPA2 + WPA3)");
