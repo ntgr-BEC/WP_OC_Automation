599c411615650b645cf83eca854a6089e00026e0
Sumanta Jena <sjena@netgear.com>
2025-04-08 18:10:00 +0530

    Screen Navigation test cases

--- com/src/test/java/webportal/NightlyBuild/PRJCBUGEN_T19277/Testcase.java
+++ com/src/test/java/webportal/NightlyBuild/PRJCBUGEN_T19277/Testcase.java
@@ -31,7 +31,7 @@ public class Testcase extends TestCaseBase {
 
     @Feature("NightlyBuild") // It's a folder/component name to make test suite more readable from Jira Test Case.
     @Story("PRJCBUGEN_T19277") // It's a testcase id/link from Jira Test Case but replace - with _.
-    @Description("Load balance") // It's a testcase title from Jira Test Case.
+    @Description("Enable and config load balancing") // It's a testcase title from Jira Test Case.
     @TmsLink("PRJCBUGEN-T19277") // It's a testcase id/link from Jira Test Case.
 
     @Test(alwaysRun = true, groups = "p2") // Use p1/p2/p3 to high/normal/low on priority
