51cdc6da8161a774f7180fbeeb05383c362dbf7f
sumanta <https://github.netgear.com/BEC/Web_Portal_Automation>
2025-04-09 18:02:39 +0530

    CFD ICP Pratik

--- com/src/test/java/webportal/CFDPayment/PRJCBUGEN_T21396/Testcase.java
+++ com/src/test/java/webportal/CFDPayment/PRJCBUGEN_T21396/Testcase.java
@@ -7,6 +7,7 @@ import java.util.Map;
 import java.util.Random;
 
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import io.qameta.allure.Description;
@@ -15,11 +16,13 @@ import io.qameta.allure.Step;
 import io.qameta.allure.Story;
 import io.qameta.allure.TmsLink;
 import testbase.TestCaseBase;
+import webportal.param.CommonDataType;
 import webportal.param.WebportalParam;
 import webportal.weboperation.AccountPage;
 import webportal.weboperation.DevicesDashPage;
 import webportal.weboperation.HamburgerMenuPage;
 import webportal.weboperation.InsightServicesPage;
+import webportal.weboperation.PostManPage;
 import webportal.weboperation.WebportalLoginPage;
 
 /**
@@ -33,6 +36,7 @@ public class Testcase extends TestCaseBase {
     int                 num               = r.nextInt(10000000);
     String              mailname          = "apwptest" + String.valueOf(num);
     Map<String, String> DeviceCreditsInfo = new HashMap<String, String>();
+    Map<String, String> paymentInfo = new HashMap<String, String>();
 
     @Feature("CFDPayment") // It's a folder/component name to make test suite more readable from Jira Test Case.
     @Story("PRJCBUGEN_T21396") // It's a testcase id/link from Jira Test Case but replace - with _.
@@ -42,6 +46,13 @@ public class Testcase extends TestCaseBase {
     @Test(alwaysRun = true, groups = "p2") // Use p1/p2/p3 to high/normal/low on priority
     public void test() throws Exception {
         runTest(this);
+    }
+
+    @BeforeMethod(alwaysRun = true)
+    public void tearUp() {
+       
+       new PostManPage().Deregister(WebportalParam.ap5serialNo);
+        
     }
 
     @AfterMethod(alwaysRun = true)
@@ -54,74 +65,82 @@ public class Testcase extends TestCaseBase {
     @Step("Test Step 1: Create IM WP account success;")
     public void step1() {
         WebportalLoginPage webportalLoginPage = new WebportalLoginPage(true);
-//        webportalLoginPage.loginByUserPassword("apwptest3110571@mailinator.com","Netgear#123");
 
         Map<String, String> accountInfo = new HashMap<String, String>();
         accountInfo.put("First Name", mailname);
-        accountInfo.put("Last Name", "T21396");
+        accountInfo.put("Last Name", "T10218");
         accountInfo.put("Email Address", mailname + "@mailinator.com");
         accountInfo.put("Confirm Email", mailname + "@mailinator.com");
         accountInfo.put("Password", "Netgear#123");
         accountInfo.put("Confirm Password", "Netgear#123");
-        accountInfo.put("Country", "United States");
+        accountInfo.put("Country", "Switzerland");
 
         new HamburgerMenuPage(false).createAccount(accountInfo);
+        
     }
+        @Step("Test Step 2: Create new location")
+        public void step2() {
+            
+            Map<String, String> locationInfo = new HashMap<String, String>();
+            locationInfo.put("Location Name", "OnBoardingTest");
+            locationInfo.put("Device Admin Password", WebportalParam.loginDevicePassword);
+            locationInfo.put("Zip Code", "4560");
+            locationInfo.put("Country", "Australia");
+            //new HamburgerMenuPage();
+            new AccountPage().addNetwork(locationInfo);
+        }
+        
+        
+        @Step("Test Step 3: Add device To the Network;")
+        public void step3() {
+            
+            new AccountPage().enterLocation("OnBoardingTest");
+            
+            Map<String, String> firststdevInfo = new HashMap<String, String>();
+           
+            
+            firststdevInfo.put("Serial Number1", WebportalParam.ap5serialNo);
+            firststdevInfo.put("MAC Address1", WebportalParam.ap5macaddress);
+            
+            System.out.println(firststdevInfo);
+     
+                    
+            new DevicesDashPage(false).addNewdummyDevice(firststdevInfo);
+            new AccountPage().enterLocation("OnBoardingTest");
+            new DevicesDashPage().waitDevicesReConnected(WebportalParam.ap5serialNo);
+          
+        }
 
-    @Step("Test Step 2: Create new location")
-    public void step2() {
-        
-        Map<String, String> locationInfo = new HashMap<String, String>();
-        locationInfo.put("Location Name", "OnBoardingTest");
-        locationInfo.put("Device Admin Password", WebportalParam.loginDevicePassword);
-        locationInfo.put("Zip Code", "STJ3310");
-        locationInfo.put("Country", "Malta");
-        new HamburgerMenuPage();
-        new AccountPage().addNetwork(locationInfo);
-    }
-    
-    
-    @Step("Test Step 3: Add device To the Network;")
-    public void step3() {
-        
-        new AccountPage().enterLocation("OnBoardingTest");
-        
-        Map<String, String> firststdevInfo = new HashMap<String, String>();
-       
-        
-        firststdevInfo.put("Serial Number1", WebportalParam.ap5serialNo);
-        firststdevInfo.put("MAC Address1", WebportalParam.ap5macaddress);
-        
-        System.out.println(firststdevInfo);
- 
-                
-        new DevicesDashPage(false).addNewdummyDevice(firststdevInfo);
-
-      
-    }
-
-    @Step("Test Step 4:  Buy insight premium subscription and cancel subscription;")
+    @Step("Test Step 4: Check buy vpn services;")
     public void step4() {
+        
+        paymentInfo = new CommonDataType().CARD_INFO;
+        
+        new HamburgerMenuPage(false).closeLockedDialog();
         new HamburgerMenuPage().gotoInsightPremiumSubscriptions();
+        
+        paymentInfo.put("First Name", "New");
+        paymentInfo.put("Last Name", "New");
+        paymentInfo.put("Email", mailname + "@mailinator.com");
+        paymentInfo.put("Device Credits Pack", "10");
+        paymentInfo.put("Buy Year", "1");
+        paymentInfo.put("Email", mailname + "@mailinator.com");
+        paymentInfo.put("Street Address", "Zuerich");
+        paymentInfo.put("City", "Zuerich");
+        paymentInfo.put("Zip", "8700");
+        paymentInfo.put("Country", "Switzerland");
+        paymentInfo.put("State", "Kuesnacht");
 
-        DeviceCreditsInfo.put("First Name", mailname);
-        DeviceCreditsInfo.put("Last Name", "T21396");
-//        DeviceCreditsInfo.put("Email", mailname + "@mailinator.com");
-        DeviceCreditsInfo.put("Street Address", "Street 4568 James Avenue");
-        DeviceCreditsInfo.put("City", "INVERNESS");
-        DeviceCreditsInfo.put("Zip", "34451");
-        DeviceCreditsInfo.put("Country", "US");
-        DeviceCreditsInfo.put("State", "Florida");
-        DeviceCreditsInfo.put("Card Number", "4112344112344113");
-        DeviceCreditsInfo.put("CVV Number", "123");
-        DeviceCreditsInfo.put("Expiration Month", "May");
-        DeviceCreditsInfo.put("Expiration Year", "2030");
-        DeviceCreditsInfo.put("Device Credits Pack", "5");
-        DeviceCreditsInfo.put("Buy Year", "1");
-        new InsightServicesPage(false).buyInsightPremiumSubscriptions(DeviceCreditsInfo);
-
+        new InsightServicesPage(false).buyInsightPremiumSubscriptions(paymentInfo);
+        
+        assertTrue(new HamburgerMenuPage(false).verifyMultipackDeviceCreditPacks(paymentInfo), "Subscriptions page display error.");
+    }
+    
+    @Step("Test Step 5: Check cancel devices credits;")
+    public void step5() {
         new HamburgerMenuPage().cancelDeviceCredits();
-        assertTrue(new HamburgerMenuPage(false).checkCancelSubscription(), "Subscription type incorrect.");
+
+        assertTrue(!new HamburgerMenuPage(false).checkDeviceCredits(paymentInfo.get("Device Credits Pack")));
     }
 
 }
