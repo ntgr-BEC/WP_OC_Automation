c0f27e8c1695c988aeb28cf3fc30e5c2455fdf06
sumanta <https://github.netgear.com/BEC/Web_Portal_Automation>
2025-05-21 17:40:10 +0530

    IM77 Location Landing page Pratik

--- com/src/test/java/webportal/ICAM_LandPageEnh/Phase_1PROEnhancements/LocationLandingPage/LocationLandingPageTable/Premium/PRJCBUGEN_T46188/Testcase.java
+++ com/src/test/java/webportal/ICAM_LandPageEnh/Phase_1PROEnhancements/LocationLandingPage/LocationLandingPageTable/Premium/PRJCBUGEN_T46188/Testcase.java
@@ -1,4 +1,4 @@
-package webportal.ICAM_LandPageEnh.Phase_1PROEnhancements.LocationLandingPage.LocationLandingPageTable.PRJCBUGEN_T46151;
+package webportal.ICAM_LandPageEnh.Phase_1PROEnhancements.LocationLandingPage.LocationLandingPageTable.Premium.PRJCBUGEN_T46188;
 
 import static org.testng.Assert.assertTrue;
 
@@ -45,11 +45,11 @@ public class Testcase extends TestCaseBase {
     Map<String, String> locationInfo = new HashMap<String, String>();
 
     @Feature("LocationLandingPageTable") // It's a folder/component name to make test suite more readable from Jira Test Case.
-    @Story("PRJCBUGEN_T46151") // It's a testcase id/link from Jira Test Case but replace - with _.
-    @Description("Verify that filters") // It's // Jira
+    @Story("PRJCBUGEN_T46188") // It's a testcase id/link from Jira Test Case but replace - with _.
+    @Description("Verify that + icon") // It's // Jira
     // Test
     // Case.
-    @TmsLink("PRJCBUGEN_T46151") // It's a testcase id/link from Jira Test Case.
+    @TmsLink("PRJCBUGEN_T46188") // It's a testcase id/link from Jira Test Case.
 
     @Test(alwaysRun = true, groups = "p1") // Use p1/p2/p3 to high/normal/low on priority
     public void test() throws Exception {
@@ -58,25 +58,42 @@ public class Testcase extends TestCaseBase {
 
     @AfterMethod(alwaysRun = true)
     public void tearDown() {
+
+        new AccountPage().deleteOneLocation("office2");
         System.out.println("start to do tearDown");
+
     }
 
     // Each step is a single test step from Jira Test Case
-    @Step("Test Step 1: Login to pro account success.")
+    @Step("Test Step 1: Login to premium account success.")
     public void step1() {
 
         WebportalLoginPage webportalLoginPage = new WebportalLoginPage(true);
-        webportalLoginPage.loginByUserPassword(WebportalParam.adminName, WebportalParam.adminPassword);
+        webportalLoginPage.defaultLogin();
 
     }
 
-    @Step("Test Step 2: Verify working of Location landing page location header filter")
+    @Step("Test Step 2: Create new location")
     public void step2() {
 
-        new OrganizationPage(false).openOrg("Netgear");
-        MyCommonAPIs.sleepi(5);
-        assertTrue(new OrganizationPage(false).verifyFilterOptionsOnLocationLandingpageHeader(),
-                "Location landing page location header filter not working");
+        HashMap<String, String> locationInfo = new HashMap<String, String>();
+        locationInfo.put("Location Name", "office2");
+        locationInfo.put("Device Admin Password", WebportalParam.loginDevicePassword);
+        locationInfo.put("Zip Code", "70184");
+        locationInfo.put("Country", "Germany");
+        new AccountPage(false).addNetwork(locationInfo);
+
+    }
+
+    @Step("Test Step 3: Verify Location dashboard Location created successfully")
+    public void step3() {
+
+        new AccountPage();
+        MyCommonAPIs.sleepi(10);
+        String clients = new OrganizationPage(false).locDashLocationsHeaderCount.shouldBe(Condition.visible).getText();
+        System.out.println("Total Locations on Location Landing page : " + clients);
+        assertTrue((clients.trim().equals("2") && ($x("//div[@col-id='locations']//p[text()='office2']").isDisplayed())),
+                "Using plus symbol location not created successfully");
 
     }
 
---
+++ com/src/test/java/webportal/ICAM_LandPageEnh/Phase_1PROEnhancements/LocationLandingPage/LocationLandingPageTable/Pro/PRJCBUGEN_T47356/TestCase.java
@@ -0,0 +1,112 @@
+package webportal.ICAM_LandPageEnh.Phase_1PROEnhancements.LocationLandingPage.LocationLandingPageTable.Pro.PRJCBUGEN_T47356;
+import static org.testng.Assert.assertTrue;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.Test;
+
+import com.codeborne.selenide.Selenide;
+
+import io.qameta.allure.Description;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Step;
+import io.qameta.allure.Story;
+import io.qameta.allure.TmsLink;
+import testbase.TestCaseBase;
+import util.MyCommonAPIs;
+import webportal.param.WebportalParam;
+import webportal.publicstep.UserManage;
+import webportal.weboperation.AccountPage;
+import webportal.weboperation.DeviceBulkOnboardingPage;
+import webportal.weboperation.OrganizationPage;
+import webportal.weboperation.WebportalLoginPage;
+import java.util.Random;
+
+public class TestCase extends TestCaseBase {
+    String organizationName = "PRJCBUGEN_T47356";
+    String locationName     = "office";
+    String warning   = "Cannot add devices from CSV file. Use CSV template column headings, and save file in .CVS format.";
+    boolean micResult = false;
+    @Feature("Create Multiple Locations Pro") // It's a folder/component name to make test suite more readable from Jira Test Case.
+    @Story("PRJCBUGEN_T47356") // It's a testcase id/link from Jira Test Case but replace - with _.
+    @Description("Test by Creating 50 locations and verify if any location goes missing") // It's a testcase title from Jira Test Case.
+    @TmsLink("PRJCBUGEN_T47356") // It's a testcase id/link from Jira Test Case.
+    
+    @Test(alwaysRun = true, groups = "p1") // Use p1/p2/p3 to high/normal/low on priority
+    
+    public void test() throws Exception {
+        runTest(this);
+    }
+
+ 
+    @AfterMethod(alwaysRun = true)
+    public void tearDown() {
+        new OrganizationPage().deleteOrganizationNew(organizationName);
+        UserManage userManage = new UserManage();
+        userManage.logout();
+        System.out.println("start to do tearDown");
+    }
+
+    // Each step is a single test step from Jira Test Case
+    @Step("Test Step 1: Login IM WP success;")
+    public void step1() {
+        WebportalLoginPage webportalLoginPage = new WebportalLoginPage(true);
+        webportalLoginPage.loginByUserPassword(WebportalParam.adminName,WebportalParam.adminPassword);
+    }
+    @Step("Test Step 2: Create a organization and location")
+    public void step2() {
+        Map<String, String> organizationInfo = new HashMap<String, String>();
+        organizationInfo.put("Name", organizationName);
+
+        OrganizationPage OrganizationPage = new OrganizationPage();
+        OrganizationPage.addOrganization(organizationInfo);
+        OrganizationPage.openOrg(organizationName);
+   
+        HashMap<String, String> locationInfo = new HashMap<String, String>();
+        locationInfo.put("Device Admin Password", WebportalParam.loginDevicePassword);
+        locationInfo.put("Zip Code", "12345");
+        locationInfo.put("Country", "India");
+        List<String> Loclist=new ArrayList<String>();  
+        int totalLocationsrequired = 50;
+        int created = 0;
+        for(int i = 1; i <=totalLocationsrequired; i++){
+//            try {
+            Random random = new Random();
+            int num = random.nextInt(100000);
+            String formatted = String.format("%05d", num);
+            Loclist.add(locationName+i);
+            locationInfo.put("Location Name", locationName+i);
+            new AccountPage(false).addNetworkforLocationCheck(locationInfo);
+            created = created+1;
+            System.out.println("Location ---------->"+created+" Created");
+            MyCommonAPIs.sleepi(5);
+//            Selenide.refresh();
+//            Selenide.sleep(5000);
+//            Selenide.refresh();
+//            Selenide.sleep(5000);
+//            assertTrue(new AccountPage(false).VerifyCreatedLocations(Loclist, totalLocationsrequired), "Missing Networks/Locations Noted");
+//            MyCommonAPIs.sleepi(5);
+//            }
+//            catch(Exception e){
+////                System.out.println("Exception => " + e.getMessage());
+////                System.out.println("Some error occured while Creation new location");
+////            }
+//            
+            }  
+        Selenide.sleep(5000);
+        Selenide.refresh();
+        Selenide.sleep(5000);
+        Selenide.refresh();
+        Selenide.sleep(5000);
+        assertTrue(new AccountPage(false).VerifyCreatedLocations(Loclist, totalLocationsrequired), "Missing Networks/Locations Noted");
+    }
+    
+  
+    
+    
+
+}
