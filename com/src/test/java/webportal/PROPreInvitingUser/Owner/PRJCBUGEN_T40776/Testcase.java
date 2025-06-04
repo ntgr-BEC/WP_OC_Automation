b5f17f95bc5c47e65a353afc19cafa3f8428b2fc
tejeshwini <tvishwanath@netgear.com>
2025-06-04 16:21:17 +0530

    Multi admin Pro Pre Invite User_teju

--- com/src/test/java/webportal/PROPreInvitingUser/Owner/PRJCBUGEN_T40776/Testcase.java
+++ com/src/test/java/webportal/PROPreInvitingUser/Owner/PRJCBUGEN_T40776/Testcase.java
@@ -33,6 +33,7 @@ public class Testcase extends TestCaseBase {
     int    num      = r.nextInt(10000000);
     String mailname = "owner" + String.valueOf(num);
     String ownMail  = mailname + "@yopmail.com";
+    String ownMail1  = mailname+"1" + "@yopmail.com";
     String organizationName = "PRJCBUGEN_T40776";
 
     @Feature("PROPreInvitingUser") // It's a folder/component name to make test suite more readable from Jira Test Case.
@@ -89,7 +90,7 @@ public class Testcase extends TestCaseBase {
         assertTrue(new HamburgerMenuPage(false).checkEmailMessageForInvitemangaerOwner(ownMail), "Not received Invitation email.");
         assertTrue(new HamburgerMenuPage(false).inviteEmailFillDateandAccept(), "Not received Invitation email.");
         Map<String, String> proAccountInfo = new HashMap<String, String>();
-        proAccountInfo.put("Confirm Email", ownMail);
+        proAccountInfo.put("Confirm Email", ownMail1);
         proAccountInfo.put("Password", "Netgear1@");
         proAccountInfo.put("Confirm Password", "Netgear1@");
         proAccountInfo.put("Country", "United States of America");
